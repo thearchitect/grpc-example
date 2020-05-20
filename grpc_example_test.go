d344c8e79b6128018fc6c3ba75489bc9770880b2
The Architect <thearchitect@themake.rs>
2020-05-20 09:38:44 +0400

    tests

--- grpc_example_test.go
+++ grpc_example_test.go
@@ -1,7 +1,55 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"fmt"
+	"net/http"
+	"testing"
+)
 
 func TestMain(m *testing.M) {
 	m.Run()
+}
+
+func TestDownload(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go RunServer(ctx)
+
+	client, err := NewClient("localhost:7532")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	name, writeTo, err := client.Download(ctx, "http://test_server:8080/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if name != "useless.blob" {
+		t.Fatalf("wrong file name: %s", name)
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+
+	if err := writeTo(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	h := fmt.Sprintf("%x", sha256.Sum256(buf.Bytes()))
+
+	if resp, err := http.Get(fmt.Sprintf("http://test_server:8080/chkhash/%s", h)); err != nil {
+		t.Error(err)
+		t.FailNow()
+	} else {
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("bad response: %d", resp.StatusCode)
+		}
+	}
+
 }
--- test-server/test_server.go
+++ test-server/test_server.go
@@ -6,28 +6,49 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"path"
 )
 
 func main() {
+	mux := http.NewServeMux()
+
+	var hashes = map[string]bool{}
+
+	mux.HandleFunc("/chkhash/", func(w http.ResponseWriter, q *http.Request) {
+		_, h := path.Split(q.URL.Path)
+		if hashes[h] {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, q *http.Request) {
+		var blob [1*1024*1024]byte
+		if n, err := rand.Read(blob[:]); err != nil {
+			panic(err)
+		} else if n != len(blob) {
+			panic(errors.New(fmt.Sprintf("size mismatch: %d != %d", n, len(blob))))
+		}
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Header().Set("Content-Disposition", "attachment;filename=useless.blob")
+		w.Header().Set("Content-Length", fmt.Sprint(len(blob)))
+
+		h := fmt.Sprintf("%x", sha256.Sum256(blob[:]))
+		w.Header().Set("X-Content-Hash", h)
+
+		hashes[h] = true
+
+		if n, err := w.Write(blob[:]); err != nil {
+			panic(err)
+		} else if n != len(blob) {
+			panic(errors.New(fmt.Sprintf("size mismatch: %d != %d", n, len(blob))))
+		}
+	})
+
 	if err := (&http.Server{
 		Addr: "0.0.0.0:8080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, q *http.Request) {
-			var blob [1*1024*1024]byte
-			if n, err := rand.Read(blob[:]); err != nil {
-				panic(err)
-			} else if n != len(blob) {
-				panic(errors.New(fmt.Sprintf("size mismatch: %d != %d", n, len(blob))))
-			}
-			w.Header().Set("Content-Type", "application/octet-stream")
-			w.Header().Set("Content-Disposition", "attachment;filename=useless.blob")
-			w.Header().Set("Content-Length", fmt.Sprint(len(blob)))
-			w.Header().Set("X-Content-Hash", fmt.Sprintf("%x", sha256.Sum256(blob[:])))
-			if n, err := w.Write(blob[:]); err != nil {
-				panic(err)
-			} else if n != len(blob) {
-				panic(errors.New(fmt.Sprintf("size mismatch: %d != %d", n, len(blob))))
-			}
-		}),
+		Handler: mux,
 	}).ListenAndServe(); err != nil {
 		panic(err)
 	}
